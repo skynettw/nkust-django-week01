a6a5ff6bf7710cafb6278135452bdf3a8f2587d0
Richard <skynet.tw@gmail.com>
2021-03-20 17:26:36 +0800

    add Post model

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.title
